fdc32edd286c423ac7841dcb743d1b5b87ed71f9
daithimcgibbon <daithimcgibbon@gmail.com>
2017-06-21 20:55:14 +0100

    basic react babel webpack app template

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var config = {
+  context: __dirname + "/src",
+  entry: "./index.js",
+
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "/dist",
+  },
+  devServer: {
+		inline: true,
+		contentBase: "./dist",
+		port: 3000
+	},
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ],
+  },
+};
+module.exports = config;
