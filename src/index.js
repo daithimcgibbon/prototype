8dac788da09dd90faa67603d137900392f98ec0e
David McGibbon <davidmcgibbon@Davids-MacBook-Pro.local>
2018-02-07 18:31:53 +0000

    Default to textbox view:

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom'
 import Prophet6 from './Prophet6'
 import storeFactory from './store'
 import { Provider } from 'react-redux'
-import Knob from './components/canvas/Knob'
+// import Knob from './components/canvas/Knob'
 
-// const store = storeFactory()
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <Prophet6 />
-//   </Provider>,
-//   document.getElementById( 'app' )
-// )
+const store = storeFactory()
 
 ReactDOM.render(
-  <Knob />,
+  <Provider store={store}>
+    <Prophet6 />
+  </Provider>,
   document.getElementById( 'app' )
 )
+
+// ReactDOM.render(
+//   <Knob />,
+//   document.getElementById( 'app' )
+// )
